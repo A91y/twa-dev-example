2c30887b8f4befffeca25e5b22aff9f4fc45bc3f
A91y <ayush.agr254@gmail.com>
2024-04-30 16:11:16 +0530

    Updates gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: {
-    outDir: "./dist",
-  },
-  base: "/<REPO>/",
+  base: "/twa-dev-example/",
 });
